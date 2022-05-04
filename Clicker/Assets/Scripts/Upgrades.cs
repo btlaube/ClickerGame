9acb378df414744641dc3ceedc9bea761d66fdf2
Ben Laube <blaube2@huskers.unl.edu>
2022-05-04 15:20:25 -0500

    Added Upgrades script, changed bot prefab
    added script for upgrades
    removed deleted script from bot prefab

---
+++ Clicker/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    //list of "upgrades" will be various functions that affect 
+}
