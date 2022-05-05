8e86e80c2d53794dedc292d07eaad59825358e0d
Ben Laube <blaube2@huskers.unl.edu>
2022-05-05 17:39:41 -0500

    started the process of creating upgrades
    Upgrades will work similarly to Bots

--- Clicker/Assets/Scripts/Upgrade.cs
+++ Clicker/Assets/Scripts/Upgrade.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 
 enum UpgradeState {UNDISCOVERED, AVAILABLE, PURCHASED}
 
-public class Upgrade : ScriptableObject
+[CreateAssetMenu(fileName = "New Upgrade", menuName = "Upgrade")]
+public abstract class Upgrade : ScriptableObject
 {
     
+    public new string name;
     public Bot bot;
     public State state;
-    
 
+    public abstract void OnActivated();
 
+}
+
+public class Bot01_Amount_Upgrade_1 : Upgrade  {
+    public override void OnActivated() {
+
+    }
 }
